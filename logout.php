a472c6dd57e238cd97e52a2e5a08c6e5ce2bb99e
Ritosom Pal <pal.ritosom@gmail.com>
2022-04-07 15:40:44 +0530

    Auto Backup to file

--- logout.php
+++ logout.php
@@ -1,11 +1,25 @@
 <?php
     session_start();
 
+    require_once('./config/DotEnv.php');
     include_once('./config/Database.php');
     include_once('./models/Users.php');
+    (new DotEnv(__DIR__ . '/config/.env'))->load();
 
     Users::Logout(new Database(), $_SESSION['user']['id']);
 
     session_destroy();
+
+    // $db = new Database;
+    // $r = $db->Backup();
+    // if($r['success']) {
+    //     $fname = explode('_',$r['fileName']);
+    //     array_pop($fname);
+    //     $fname = implode('_',$fname);
+    //     $file = $r['filePath'].'/'.$r['fileName'].'.zip';
+    //     $newfile = $_ENV['AUTO_BACKUP_PATH'].'/'.$fname.'.db';
+    //     copy($file, $newfile);
+    //     unlink($file);
+    // }
 
     header('location: ./login');
